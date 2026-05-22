e230ba34ba16c84cae9f29a7364ba431aa32538e
tomas-vasquez <tomasdetloging@gmail.com>
2026-05-22 21:24:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import main from "@/data/main.json";
+import business from "@/data/business.json";
+import about from "@/data/about.json";
+import services from "@/data/services.json";
+import ubication from "@/data/ubication.json";
+import frequentQuestions from "@/data/frequentQuestions.json";
+
+export default {
+  main,
+  business,
+  about,
+  services,
+  ubication,
+  frequentQuestions,
+};
---
+++ index.ts
@@ -0,0 +1,8 @@
+import main from "./main.json";
+import business from "./business.json";
+import about from "./about.json";
+import services from "./services.json";
+import ubication from "./ubication.json";
+import frequentQuestions from "./frequentQuestions.json";
+
+export default { main, business, about, services, ubication, frequentQuestions };
